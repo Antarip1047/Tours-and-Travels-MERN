a8f8352c2240c1d4593c7cbfaf8a3f108358f0a9
ANTARIP SAHA <antaripsaha2018@gmail.com>
2024-04-29 22:41:54 +0530

    13.40 before header css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 //import logo from './logo.svg';
 import './App.css';
+import Layout from './components/Layout/Layout';
 
 function App() {
-  return (
-    //<div className="App">
-      <h1>Hello World</h1>
-  );
+  return <Layout/>
 }
 
 export default App;
