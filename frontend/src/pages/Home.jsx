bc8feb16b8ce805b309078281592b4cba6b83ffb
ANTARIP SAHA <antaripsaha2018@gmail.com>
2024-04-30 22:49:50 +0530

    1.24.00 mins

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,10 +5,13 @@ import heroImg from '../assets/images/hero-img01.jpg'
 import heroImg02 from '../assets/images/hero-img02.jpg'
 import heroVideo from '../assets/images/hero-video.mp4'
 import worldImg from '../assets/images/world.png'
+import experienceImg from '../assets/images/experience.png'
 import Subtitle from '../shared/Subtitle';
 import SearchBar from '../shared/SearchBar';
 import ServiceList from '../services/ServiceList';
 import FeaturedTourList from '../components/Featured-tours/FeaturedTourList';
+import MasonryImagesGallery from '../Image-gallery/MasonryImagesGallery';
+import Testimonials from '../components/Testimonial/Testimonials';
 
 const Home = () => {
   return( 
@@ -78,6 +81,83 @@ const Home = () => {
       </Container>
     </section>
     {/* =============== feature tour end===============*/}
+
+    {/* =============== experience section start===============*/}
+    <section>
+      <Container>
+        <Row>
+          <Col lg='6'>
+            <div className="experience_content">
+              <Subtitle subtitle={'Experience'}/>
+
+              <h2>
+                With our all experience <br />we will serve you
+              </h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                <br />
+                Necessitatibus, temporibus!
+              </p>
+            </div>
+
+            <div className="counter_wrapper d-flex align-items-center gap-5">
+              <div className="counter_box">
+                <span>12k+</span>
+                <h6>Successful trip</h6>
+              </div>
+              <div className="counter_box">
+                <span>2k+</span>
+                <h6>Regular Clients</h6>
+              </div>
+              <div className="counter_box">
+                <span>15</span>
+                <h6>years experience</h6>
+              </div>
+            </div>
+          </Col>
+          <Col lg='6'>
+            <div className="experience_img">
+              <img src={experienceImg} alt="" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    {/* =============== experience section end===============*/}
+
+    {/* =============== gallery section start===============*/}
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12'>
+            <Subtitle subtitle={'Gallery'}/>
+            <h2 className="gallery_title">
+              Visit our customers tour gallery
+            </h2>
+          </Col>
+          <Col lg='12'>
+            <MasonryImagesGallery/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    {/* =============== gallery section end===============*/}
+
+    {/* =============== testimonial section start===============*/}
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12'>
+          <Subtitle subtitle={'Fans Love'}/>
+          <h2 className="testimonial_title">What our fans say about us</h2>
+          </Col>
+          <Col lg='12'>
+            <Testimonials/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+    {/* =============== testimonial section end===============*/}
     </>
     )
 }
